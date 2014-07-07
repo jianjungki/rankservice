d46237336ec27252b903c33effc7d111da92296f
jianjungki <jianjungki@sina.com>
2014-07-07 23:32:40 +0800

    change the main.js
    change the main.js for the gamescore search

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,20 @@
-require("cloud/app.js");
+//require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
-AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+AV.Cloud.define("rankinfo", function(request, response) {
+	
+  var query = new AV.Query("GameScore");
+  query.near("score", request.params.score);
+  
+  query.find({
+    success: function(results) {
+      var sum = 0;
+      for (var i = 0; i < results.length; ++i) {
+        sum += results[i].get("score");
+      }
+      response.success();
+    },
+    error: function() {
+      response.error("rankinfo find failed");
+    }
 });
