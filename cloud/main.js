80f2f5746dbc0fe11105e7185613237edd00407c
jianjungki <jianjungki@sina.com>
2014-07-08 00:02:59 +0800

    fixed the bug
    fixed the bug

--- cloud/main.js
+++ cloud/main.js
@@ -5,12 +5,8 @@ AV.Cloud.define("rankinfo", function(request, response) {
 
   var better = new AV.Query("GameScore");
   better.greaterThan("score", request.params.score);
-
-	var lower = new AV.Query("GameScore");
-	lower.lessThan("score", request.params.score);
   
-  var mainQuery = AV.Query.or(better, lower);
-  mainQuery.find({
+  better.find({
     success: function(results) {
       for (var i = 0; i < results.length; ++i) {
         sum += results[i].get("score");
