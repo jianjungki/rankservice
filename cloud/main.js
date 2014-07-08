6a91f16ca446cbb2a84677579673721df77f118b
jianjungki <jianjungki@sina.com>
2014-07-08 11:07:29 +0800

    fixed
    fixed

--- cloud/main.js
+++ cloud/main.js
@@ -5,44 +5,25 @@ AV.Cloud.define("rankinfo", function(request, response) {
 
    var better = new AV.Query("GameScore");
    better.greaterThanOrEqualTo("score", request.params.score);
-   better.limit(1);
    
    var lower = new AV.Query("GameScore");
    lower.lessThan("score", request.params.score);
-   lower.limit(1);
    
-   var greater;
-   var less;
-   better.find({
+   var mainQuery = AV.Query.or(lotsOfWins, fewWins);
+   mainQuery.find({
 		success: function(results) {
+	      var ret_arr = new Array();
 		  for (var i = 0; i < results.length; ++i) {
-			greater = {
-				name: '222', 
-				score: 50
+			var element = {
+				name: results[i].get("playerName"), 
+				score: results[i].get("score")
 			};
-			response.success(JSON.stringify(greater));
+			ret_arr.push(element);
 		  }
+		  response.success(JSON.stringify(ret_arr));
 		},
 		error: function() {
 		  response.error("rankinfo find failed");
 		}
    })
-   
-   lower.find({
-		success: function(results) {
-		  for (var i = 0; i < results.length; ++i) {
-			less = {
-				name: '111', 
-				score: 30
-			};
-			response.success(JSON.stringify(less));
-		  }
-		},
-		error: function() {
-		  response.error("rankinfo find failed");
-		}
-   })
-   
-   var ret_arr = new Array(greater ,less);
-   response.success(JSON.stringify(ret_arr));
 });
