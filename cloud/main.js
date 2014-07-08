9d4d4fc45bc111c7960ec40b2f46b7c161dac1cc
jianjungki <jianjungki@sina.com>
2014-07-08 10:49:20 +0800

    add the change logic
    fixed

--- cloud/main.js
+++ cloud/main.js
@@ -3,19 +3,44 @@
 // For example:
 AV.Cloud.define("rankinfo", function(request, response) {
 
-  var better = new AV.Query("GameScore");
-  better.greaterThan("score", request.params.score);
-  
-  better.find({
-    success: function(results) {
-	  var sum = 0;
-      for (var i = 0; i < results.length; ++i) {
-        sum += results[i].get("score");
-      }
-      response.success(sum);
-    },
-    error: function() {
-      response.error("rankinfo find failed");
-    }
-	})
+   var better = new AV.Query("GameScore");
+   better.greaterThanOrEqualTo("score", request.params.score);
+   better.limit(1);
+   
+   var lower = new AV.Query("GameScore");
+   lower.lessThan("score", request.params.score);
+   lower.limit(1);
+   
+   var greater;
+   var less;
+   better.find({
+		success: function(results) {
+		  for (var i = 0; i < results.length; ++i) {
+			greater = {
+				name: results[i].get("playerName"), 
+				score: results[i].get("score")
+			};
+		  }
+		},
+		error: function() {
+		  response.error("rankinfo find failed");
+		}
+   })
+   
+   better.find({
+		success: function(results) {
+		  for (var i = 0; i < results.length; ++i) {
+			less = {
+				name: results[i].get("playerName"), 
+				score: results[i].get("score")
+			};
+		  }
+		},
+		error: function() {
+		  response.error("rankinfo find failed");
+		}
+   })
+   
+   var ret_arr = new Array(greater ,less);
+   response.success(JSON.stringify(ret_arr));
 });
